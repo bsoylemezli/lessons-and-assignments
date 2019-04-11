8bec5023793b221737d2c779153fee2c7507cd58
Shubham Saini <shubham1996saini@gmail.com>
2019-04-11 07:49:40 +0530

    Add files via upload

---
+++ Assignment1.py
@@ -0,0 +1,25 @@
+#print the second element from array
+fruits = ["apple", "banana", "cherry"]
+print(      )
+
+____________________________________________________
+
+#print multiple 5 till 10 by using range
+#do same as I have done by printing multiple of 2 using range(2,10,2)
+
+____________________________________________
+
+#Write a python code to search 'j' in array x and print YES if available or else NO
+x=['f','g','t','h','j','w','k','p']
+
+______________________________________________
+
+#Change the value from "apple" to "kiwi", in the x.
+#fill the dotted missing logic
+#HINT --> use assignment operator
+
+x=["apple","orange","banana","grapes"]
+
+_ _ _ _ _ = _ _ _ _ _
+
+_____________________________________________________________________
